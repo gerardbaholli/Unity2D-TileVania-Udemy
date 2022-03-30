7fe3a03f2a8517f4892adde46d83935b71546adf
gerardbaholli <gerardbaholli@live.it>
2022-03-31 01:09:48 +0200

    Add lessons from 79 to 83

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+    Vector2 moveInput;
+    Rigidbody2D myRigidbody;
+    Transform myTransform;
+    Animator myAnimator;
+
+    private void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myTransform = GetComponent<Transform>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Run();
+        FlipSprite();
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+            myTransform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
+    }
+
+    private void Run()
+    {
+        Vector2 playerVelocity = new(moveInput.x * runSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
+    }
+
+    private void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+        Debug.Log(moveInput);
+    }
+
+    private void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            myRigidbody.velocity += new Vector2(0f, jumpSpeed);
+        }
+    }
+
+}
