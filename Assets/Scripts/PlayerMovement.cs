b1fd0011bd15bb52c13fd3f3eacf5707dd629f08
Gerard Baholli <gerardbaholli@live.it>
2022-12-01 00:30:08 +0100

    Add lessons from 84 to 88

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,22 +8,30 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float runSpeed = 5f;
     [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 5f;
+
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
     Transform myTransform;
     Animator myAnimator;
+    CapsuleCollider2D myCapsuleCollider;
+
+    float gravityScaleAtStart;
 
     private void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         myTransform = GetComponent<Transform>();
         myAnimator = GetComponent<Animator>();
+        myCapsuleCollider = GetComponent<CapsuleCollider2D>();
+        gravityScaleAtStart = myRigidbody.gravityScale;
     }
 
     private void Update()
     {
         Run();
         FlipSprite();
+        ClimbLadder();
     }
 
     private void FlipSprite()
@@ -31,6 +39,23 @@ public class PlayerMovement : MonoBehaviour
         bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
         if (playerHasHorizontalSpeed)
             myTransform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
+    }
+
+    private void ClimbLadder()
+    {
+        if (!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        {
+            myAnimator.SetBool("isClimbing", false);
+            myRigidbody.gravityScale = gravityScaleAtStart;
+            return;
+        }
+
+        myRigidbody.gravityScale = 0f;
+        Vector2 climbVelocity = new Vector2(myRigidbody.velocity.x, moveInput.y * climbSpeed);
+        myRigidbody.velocity = climbVelocity;
+
+        bool playerHasVerticalSpeed = Mathf.Abs(myRigidbody.velocity.y) > Mathf.Epsilon;
+        myAnimator.SetBool("isClimbing", playerHasVerticalSpeed);
     }
 
     private void Run()
@@ -50,6 +75,9 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnJump(InputValue value)
     {
+        if (!myCapsuleCollider.IsTouchingLayers(LayerMask .GetMask("Ground")))
+            return;
+
         if (value.isPressed)
         {
             myRigidbody.velocity += new Vector2(0f, jumpSpeed);
